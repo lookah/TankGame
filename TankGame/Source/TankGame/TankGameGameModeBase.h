6a9f3973ae2c9f5ec4fe4d3854b97e65926b7273
Luka Gudic <looka.gudic@gmail.com>
2017-08-26 17:46:58 +0200

    TankGame - dodan landscape - potrebno odrediti landscape constraints

---
+++ TankGame/Source/TankGame/TankGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API ATankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
